0b02f2194cf11148f02907b51341003db80739b6
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-02 18:38:15 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = "https://camdkqqiftwlqmklllyl.supabase.co";
+const supabaseAnonKey = "sb_publishable_HwUBclRI-4DQb_02rNoC1w_Gc8ETnal";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
