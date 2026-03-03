4c92e6941ca4c4da08976bfee9e8ceedc9d236bb
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-03 21:00:49 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
@@ -1106,6 +1108,7 @@ return hasAccess ? (
   <AccessPage onAccessGranted={() => setHasAccess(true)} />
 );
 }
+
 
 
 
