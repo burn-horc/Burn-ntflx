821ef78c41d0b74e92df973ddc20b57d7651cad7
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-07 17:54:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -768,11 +768,12 @@ if (fs.existsSync(clientDist)) {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running");
 });
 
-module.exports = app;
 
 
 
